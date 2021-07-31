924f573b44a5c2b57f1afd0fa455078da09fa911
Sajal <sajmandal@deloitte.com>
2021-07-31 19:25:12 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Banner from './components/Banner';
+import Header from './components/Header';
+import Courses from './components/Courses';
+import CartDetails from './components/CartDetails';
+
+
+interface IState {
+  course: {
+    courseName: string
+    trackLead: string 
+    price: number
+  }[]
+}
 
 function App() {
+
+  const [course, setCourse] = useState<IState["course"]>([
+    {
+      courseName: "Responsive Design Course XYZ How to design responsive templates",
+      trackLead: "Joseph Marie",
+      price: 563
+    },
+    {
+      courseName: "Responsive Design Course XYZ How to design responsive templates",
+      trackLead: "Joseph Marie",
+      price: 563
+    },
+    {
+      courseName: "Responsive Design Course XYZ How to design responsive templates",
+      trackLead: "Joseph Marie",
+      price: 563
+    },
+    {
+      courseName: "Responsive Design Course XYZ How to design responsive templates",
+      trackLead: "Joseph Marie",
+      price: 563
+    },
+    {
+      courseName: "Responsive Design Course XYZ How to design responsive templates",
+      trackLead: "Joseph Marie",
+      price: 563
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Banner/>
+      <div className="body-content">
+        <Courses course={course}/>
+        <CartDetails/>
+      </div>
+      
+      
+      
+
     </div>
   );
 }
